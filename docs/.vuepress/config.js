f6bbcc394b6a289dcb6c1045d14671d293232ec6
Muhammad Wahyudin <wahyudin@recharge.id>
2019-07-30 12:23:54 +0700

    Fix: Disable VueApexCharts component that cause build to fail
    Upgrade Vuepress to 1.0.3. Turns out that apexcharts is the culprit

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,14 +3,29 @@ module.exports = {
     title: 'Muhammad Wahyudin\'s Page',
     description: 'Software Developer - Instrumentation & Electronics Physicist - Technology Enthusiast',
     head: [
-        ['link', { rel: 'icon', href: 'http://hyuwah.github.io/assets/favicon/favicon.ico?v=xQzbabgwXG' }],
-        ['script',{src:`https://sethusenthil.com/Product-Sans/api/font.js`}],
-        ['script',{src:`https://cdn.jsdelivr.net/npm/vue-scroll-reveal@1.0.11/dist/vue-scroll-reveal.min.js`}],
-        ['link', {rel:'stylesheet', href:"https://use.fontawesome.com/releases/v5.6.3/css/all.css"}],
+        ['link', {
+            rel: 'icon',
+            href: 'http://hyuwah.github.io/assets/favicon/favicon.ico?v=xQzbabgwXG'
+        }],
+        ['script', {
+            src: `https://sethusenthil.com/Product-Sans/api/font.js`
+        }],
+        // ['script', {
+        //     src: `https://cdn.jsdelivr.net/npm/vue-scroll-reveal@1.0.11/dist/vue-scroll-reveal.min.js`
+        // }],
+        ['link', {
+            rel: 'stylesheet',
+            href: `https://cdn.jsdelivr.net/npm/vuetify@1.x/dist/vuetify.min.css`
+        }],
+        ['link', {
+            rel: 'stylesheet',
+            href: "https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+        }],
     ],
-    themeConfig:{
-        nav:[
-            { text:'PORTFOLIO', link:'/portfolio/'}
-        ]
+    themeConfig: {
+        nav: [{
+            text: 'PORTFOLIO',
+            link: '/portfolio/'
+        }]
     },
 }
