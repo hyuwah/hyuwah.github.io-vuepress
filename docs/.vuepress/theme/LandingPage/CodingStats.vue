39dfd7d73cc15b13f7b7cb03c1f190b5f58f183a
Muhamad Wahyudin <m.wahyudin@pegipegi.com>
2020-02-17 10:36:54 +0700

    Rework Landing Page
    - wip coding stats using vue apex charts

--- docs/.vuepress/theme/LandingPage/CodingStats.vue
+++ docs/.vuepress/theme/LandingPage/CodingStats.vue
@@ -1,62 +1,172 @@
 <template>
-  <!-- <v-flex xs12 sm10 md8 offset-sm1 offset-md2 class="pb-5 mt-5 mb-5">
-    <v-layout v-scroll-reveal.reset="{delay:250}" row class="mb-4" align-end>
+  <v-flex xs12 sm10 md8 offset-sm1 offset-md2 class="pb-5 mt-5 mb-5">
+    <v-layout v-scroll-reveal.reset="{ delay: 250 }" row class="mb-4" align-end>
       <h2 class="display-2 white--text">Coding Stats</h2>
-    </v-layout> -->
+    </v-layout>
     <!-- Pake Card? -->
-    <!-- <v-flex xs12 md8 lg6 offset-md2 offset-lg3> -->
+    <v-layout row wrap>
       <!-- Languages -->
-      <!-- <apexcharts width="100%" type="pie" :options="langOptions" :series="langSeries"/>
-      <apexcharts width="100%" type="donut" :options="langOptions" :series="langSeries"/> -->
-      <!-- <div>
-        <div>{{this.langOptions.labels}} {{this.langResponse}}</div>
-      </div> -->
-    <!-- </v-flex>
-  </v-flex> -->
+      <v-flex xs12 md8 lg5 ma-1>
+        <v-card>
+          <v-card-title primary-title>
+            <h1>Languages</h1>
+          </v-card-title>
+          <v-card-text>
+            <apexcharts
+              type="radialBar"
+              :options="chartOptions"
+              :series="langSeries"
+            />
+          </v-card-text>
+        </v-card>
+      </v-flex>
+      <!-- Activity -->
+      <v-flex xs12 md8 lg5 ma-1>
+        <v-card>
+          <v-card-title primary-title>
+            <h1>Activity</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-layout column text-xs-center>
+              <v-flex
+                xs4
+                sm2
+                v-for="caItem in caResponse"
+                :key="caItem.range.date"
+              >
+                <div>
+                  {{ caItem.range.text }} : {{ caItem.grand_total.text }}
+                </div>
+              </v-flex>
+            </v-layout>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-flex>
 </template>
 
 <script>
-// import VueApexCharts from "vue-apexcharts";
-// import axios from "axios";
+import VueApexCharts from "vue-apexcharts";
+import axios from "axios";
 
-// export default {
-//   name: "CodingStats",
-//   components: {
-//     apexcharts: VueApexCharts,
-//   },
-//   data: function() {
-//     return {
-//       langResponse: {},
-//       langSeries: []
-//     };
-//   },
-//   computed: {
-//     langOptions(){
-//       return {
-//         labels: [],
-//         dataLabels: {
-//             formatter: function (val) {
-//               return val.toFixed(2) + "%"
-//             },
-//           },
-//       }
-//     },
-//   },
-//   beforeMount() {
-//     // Enable cors chrome addon dulu biar ga network error (tapi nanti ga bisa youtube)
-//     // Languages
-//     axios
-//       .get(
-//         "https://wakatime.com/share/@hyuwah/9235ab47-5904-489c-9a70-2c141cbe079b.json"
-//       )
-//       .then(res => {
-//         this.langResponse = res.data.data;
-//         this.langSeries = this.langResponse.map(item => item.percent);
-//         this.langOptions.labels = this.langResponse.map(item => item.name);
-//       })
-//       .catch(err => {
-//         this.langResponse = err;
-//       });
-//   }
-// };
+export default {
+  name: "CodingStats",
+  components: {
+    apexcharts: VueApexCharts
+  },
+  data: function() {
+    return {
+      langResponse: {},
+      langSeries: [],
+      langLabels: [],
+      caResponse: {},
+      caSeries: []
+    };
+  },
+  computed: {
+    langOptions() {
+      return {
+        labels: [],
+        dataLabels: {
+          formatter: function(val) {
+            return val.toFixed(2) + "%";
+          }
+        }
+      };
+    },
+    chartOptions() {
+      return {
+        chart: {
+          type: "radialBar"
+        },
+        plotOptions: {
+          radialBar: {
+            offsetY: 0,
+            startAngle: 0,
+            endAngle: 270,
+            hollow: {
+              margin: 5,
+              size: "30%",
+              background: "transparent",
+              image: undefined
+            },
+            dataLabels: {
+              name: {
+                show: false
+              },
+              value: {
+                show: false
+              }
+            }
+          }
+        },
+        labels: this.langLabels,
+        legend: {
+          show: true,
+          floating: true,
+          fontSize: "14px",
+          position: "left",
+          offsetX: 64,
+          offsetY: 0,
+          labels: {
+            useSeriesColors: true
+          },
+          markers: {
+            size: 0
+          },
+          formatter: function(seriesName, opts) {
+            return seriesName + ":  " + opts.w.globals.series[opts.seriesIndex];
+          },
+          itemMargin: {
+            horizontal: 3
+          }
+        },
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              legend: {
+                show: true,
+                floating: true,
+                fontSize: "9px",
+                position: "left",
+                offsetX: -10,
+                offsetY: -20
+              }
+            }
+          }
+        ]
+      };
+    }
+  },
+  beforeMount() {
+    // Enable cors chrome addon dulu biar ga network error (tapi nanti ga bisa youtube)
+    // Languages
+    axios
+      .get(
+        "https://wakatime.com/share/@hyuwah/9235ab47-5904-489c-9a70-2c141cbe079b.json"
+      )
+      .then(res => {
+        this.langResponse = res.data.data;
+        this.langSeries = this.langResponse.map(item => item.percent);
+        this.langLabels = this.langResponse.map(item => item.name);
+      })
+      .catch(err => {
+        this.langResponse = err;
+      });
+
+    // Coding Activity
+    axios
+      .get(
+        "https://wakatime.com/share/@hyuwah/0c9861c2-73ad-4f05-9c82-abe115b163e1.json"
+      )
+      .then(res => {
+        this.caResponse = res.data.data.reverse();
+      })
+      .catch(err => {
+        this.caResponse = err;
+      });
+  }
+};
 </script>
--- docs/.vuepress/theme/LandingPageLayout.vue
+++ docs/.vuepress/theme/LandingPageLayout.vue
@@ -4,17 +4,15 @@
     <v-parallax
       id="heroParallax"
       src="https://res.cloudinary.com/hyuwah-github-io/image/upload/v1566016247/hyuwah.github.io/v2-header-alt.webp"
-    >
-      <v-layout align-center column justify-center class="ma-3">
-        <a href="https://www.credential.net/62f48fc6-dfef-4604-a900-b59a5d2cc17c" target="_blank" rel="noopener">
-         <v-avatar size="156px" class="mb-3">
-            <v-img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/14516874"/>
-         </v-avatar>
-        </a>
-        <h1 class="display-2 font-weight-bold mb-3 text-xs-center">MUHAMMAD WAHYUDIN</h1>
+      >
+      <v-layout align-center column justify-center class="ma-3 mb-1">
+        <v-avatar size="96px">
+          <v-img src="https://hyuwah.github.io/assets/images/pp-square.webp"/>
+        </v-avatar>
+        <h1 class="display-2 font-weight-bold ma-3 mb-3 text-xs-center">MUHAMMAD WAHYUDIN</h1>
         <h3
           class="font-weight-light text-xs-center"
-        >Software Developer - Instrumentation &amp; Electronics Physicist - Technology Enthusiast</h3>
+        >Software Engineer -- Instrumentation &amp; Electronics Physicist</h3>
       </v-layout>
     </v-parallax>
 
@@ -34,18 +32,21 @@
             <v-hover>
               <v-card slot-scope="{hover}" :class="`card-rounded elevation-${hover?12:2}`">
                 <v-card-title primary-title>
-                  <v-layout row justify-center>
-                    <v-avatar size="96px">
-                      <v-img src="https://hyuwah.github.io/assets/images/pp-square.webp"/>
-                    </v-avatar>
+                  <v-layout column align-center>
+                    <h1 class="primary--text mb-2">Hello There!</h1>
+                  <h2
+                    class="font-weight-light pl-4 pr-4 text-sm-center"
+                  >I'm Wahyu, physics graduate turned Google certified Associate Android Developer. Currently in love with Kotlin, but also have a crush on Flutter</h2>
                   </v-layout>
                 </v-card-title>
                 <v-card-text class="pr-4 pl-4 pb-4 pt-0 text-xs-center">
-                  <h1 class="primary--text">Hello There!</h1>
-                  <h2
-                    class="font-weight-light"
-                  >I'm Wahyu, Android Developer with bachelor's degree in Physics from Universitas Padjadjaran.</h2>
-                  <br>
+                  <v-layout column wrap class="mb-3">
+                    <a href="https://www.credential.net/62f48fc6-dfef-4604-a900-b59a5d2cc17c" target="_blank" rel="noopener">
+                      <v-avatar size="156px" class="mb-3">
+                          <v-img src="https://api.accredible.com/v1/frontend/credential_website_embed_image/badge/14516874"/>
+                      </v-avatar>
+                    </a>
+                  </v-layout>
                   <v-layout row wrap justify-center>
                     <v-flex
                       xs4
@@ -66,14 +67,6 @@
                         </a>
                     </v-flex>
                   </v-layout>
-
-                  <!-- <a href="https://sourcerer.io/hyuwah" target="_blank">
-                    <img
-                      src="https://sourcerer.io/icons/logo-sharing.svg"
-                      height="48px"
-                      alt="Sourcerer"
-                    >
-                  </a>-->
                 </v-card-text>
               </v-card>
             </v-hover>
